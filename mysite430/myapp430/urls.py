9167f7c525d805a580687dd3d597bbf9f7d9e7f9
LamaHasbini <125820185+LamaHasbini@users.noreply.github.com>
2024-04-03 14:35:21 +0300

    v4

--- mysite430/myapp430/urls.py
+++ mysite430/myapp430/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
-from django.conf.urls import url
+##from django.conf.urls import url
+from django.urls import re_path as url
 from .views import *
 
 urlpatterns = [
